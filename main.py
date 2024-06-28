eb1d62d99418b6db84b104e7c6db765e058ec5b6
Finn Fancher <fancherfinn@gmail.com>
2024-06-28 16:37:21 -0700

    update main.py

--- main.py
+++ main.py
@@ -6,13 +6,22 @@ def main():
     Code Your Program here
     ########################################
     """
+    for i in range(5):
+        numbers.append((input("Enter number: ")))
+        
+    min = numbers[0]
+    max = numbers[0]
+    
+    for i in range(1, 5):
+        if(min > numbers[i]):
+            min = numbers[i]
 
-    print(*numbers)
-    print(maxval, minval)
+    print(numbers)
+    print(max, min)
     ########################################
     # Do not delete the return statement
     ########################################
-    return numbers, maxval, minval
+    return numbers, max, min
 
 
 if __name__ == '__main__':
